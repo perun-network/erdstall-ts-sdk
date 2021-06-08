21fa9a4b5a0c3d68cc6505ad9cd0787f9e5c7869
Norbert Dzikowski <norbert@perun.network>
2021-06-08 11:08:52 +0200

    api: Fix formatting and add json test for balanceproofs

--- src/api/object.impl.ts
+++ src/api/object.impl.ts
@@ -11,22 +11,21 @@ ErdstallObject.fromJSON = (js: any): ErdstallObject => {
 	let data = JSON.stringify(js.data);
 
 	switch (js.type) {
-	case "Transaction":
-		return TypedJSON.parse(data, Transaction)!;
-	case "Subscribe":
-		return TypedJSON.parse(data, calls.Subscribe)!;
-	case "GetAccount":
-		return TypedJSON.parse(data, calls.GetAccount)!;
-	case "TxReceipt":
-		return TypedJSON.parse(data, responses.TxReceipt)!;
-	case "ClientConfig":
-		return TypedJSON.parse(data, responses.ClientConfig)!;
-	case "AccountResponse":
-		return TypedJSON.parse(data, responses.Account)!;
-	case "BalanceProof":
-		console.info("Received BalanceProof");
-		return TypedJSON.parse(data, responses.BalanceProof)!;
-	default:
-		throw new Error(`unknown type "${js.type}"`);
+		case "Transaction":
+			return TypedJSON.parse(data, Transaction)!;
+		case "Subscribe":
+			return TypedJSON.parse(data, calls.Subscribe)!;
+		case "GetAccount":
+			return TypedJSON.parse(data, calls.GetAccount)!;
+		case "TxReceipt":
+			return TypedJSON.parse(data, responses.TxReceipt)!;
+		case "ClientConfig":
+			return TypedJSON.parse(data, responses.ClientConfig)!;
+		case "AccountResponse":
+			return TypedJSON.parse(data, responses.Account)!;
+		case "BalanceProof":
+			return TypedJSON.parse(data, responses.BalanceProof)!;
+		default:
+			throw new Error(`unknown type "${js.type}"`);
 	}
 };
