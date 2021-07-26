d32ce043ab123fc9289d3268e98def6087ca55df
Sebastian Stammler <seb@perun.network>
2021-07-26 15:45:57 +0200

    Consistently import Assets from ledger/assets package

--- src/ledger/account.ts
+++ src/ledger/account.ts
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: Apache-2.0
 "use strict";
 
-import * as assets from "./assets";
 import { jsonObject, jsonMember } from "typedjson";
 import { BigInteger } from "#erdstall/api/util";
+import { Assets } from "#erdstall/ledger/assets";
 
 @jsonObject
 export class Account {
 	@jsonMember(BigInteger) nonce: BigInteger;
-	@jsonMember(assets.Assets) values: assets.Assets;
-	@jsonMember(assets.Assets) locked: assets.Assets;
+	@jsonMember(Assets) values: Assets;
+	@jsonMember(Assets) locked: Assets;
 
-	constructor(nonce: bigint, values: assets.Assets, locked: assets.Assets) {
+	constructor(nonce: bigint, values: Assets, locked: Assets) {
 		this.nonce = new BigInteger(nonce);
 		this.values = values;
 		this.locked = locked;
