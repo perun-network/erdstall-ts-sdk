5946f801c2abbbcc8f24c7effc4d4d1e865a1e9b
Sebastian Stammler <seb@perun.network>
2021-07-30 15:13:45 +0200

    test: Add newRandomWallet and use it in existing tests

--- src/api/sig.spec.ts
+++ src/api/sig.spec.ts
@@ -3,20 +3,18 @@
 
 import "reflect-metadata";
 import { expect } from "chai";
-import { MockProvider } from "ethereum-waffle";
 import { Address } from "#erdstall/ledger";
 import * as test from "#erdstall/test";
 
-const gProvider = new MockProvider();
-const wallets = gProvider.getWallets();
-
 describe("Signatures", () => {
 	const rng = test.NewPrng();
+	const wallet = test.newRandomWallet(rng);
+
 	it("works for transactions", async () => {
 		const contract = test.NewRandomAddress(rng);
 		const tx = test.NewRandomTransfer(rng, 1);
-		tx.sender = Address.fromString(wallets[0].address);
-		await tx.sign(contract, wallets[0]);
+		tx.sender = Address.fromString(wallet.address);
+		await tx.sign(contract, wallet);
 
 		expect(tx.verify(contract)).to.be.true;
 	});
@@ -24,8 +22,8 @@ describe("Signatures", () => {
 	it("works for mints", async () => {
 		const contract = test.NewRandomAddress(rng);
 		const tx = test.NewRandomMint(rng);
-		tx.sender = Address.fromString(wallets[0].address);
-		await tx.sign(contract, wallets[0]);
+		tx.sender = Address.fromString(wallet.address);
+		await tx.sign(contract, wallet);
 		expect(tx.verify(contract)).to.be.true;
 	});
 });
