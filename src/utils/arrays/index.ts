0e4dbd054258cec0fc9f70f38e330647fbb70595
Sebastian Stammler <seb@perun.network>
2021-08-02 12:27:22 +0200

    utils/arrays: Add equalArray function to new package utils/arrays

---
+++ src/utils/arrays/index.ts
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+
+export type TypedArray = Int8Array | Uint8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array;
+
+export function equalArray<T extends TypedArray>(x: T, y: T): boolean {
+	return (x.length === y.length)
+		&& (x.every((a, i) => y[i] === a));
+}
