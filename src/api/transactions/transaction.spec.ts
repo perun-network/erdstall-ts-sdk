f4355f1162a99c22dc6999d3da69aa7a78afceef
Norbert Dzikowski <norbert@perun.network>
2021-09-21 11:16:06 +0200

    transactions.spec: Boyscout superfluous import

--- src/api/transactions/transaction.spec.ts
+++ src/api/transactions/transaction.spec.ts
@@ -6,7 +6,6 @@ import { describe, it } from "mocha";
 import { TypedJSON } from "typedjson";
 import { Call, Result, ErdstallObject } from "#erdstall/api";
 import { expect } from "chai";
-import { BalanceProofs } from "#erdstall/api/responses";
 import { Transaction } from "./transaction";
 import { Trade } from "./trade";
 import { Address } from "#erdstall/ledger";
