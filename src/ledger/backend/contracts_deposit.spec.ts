192bfa5a6a88b9cfc25cabfbaf888bfa9af0671f
Norbert Dzikowski <norbert@perun.network>
2021-09-28 15:04:15 +0200

    ledger/backend: Add testcase for ERC721 deposits

--- src/ledger/backend/contracts_deposit.spec.ts
+++ src/ledger/backend/contracts_deposit.spec.ts
@@ -4,38 +4,58 @@
 import { expect } from "chai";
 
 import { Wallet } from "ethers";
-import { ETHZERO, Amount } from "#erdstall/ledger/assets";
+import { ETHZERO, Amount, Asset } from "#erdstall/ledger/assets";
 import { Address } from "#erdstall/ledger";
 import {
 	makeETHDepositCalls,
 	makeERC20DepositCalls,
 	makeERC721DepositCalls,
 } from "./contracts_deposit";
+import { DepositerCallsFactory } from "./tokenmanager";
+import { PerunArt__factory } from "./contracts/factories/PerunArt__factory";
 
 import { Enviroment, setupEnv } from "#erdstall/test/ledger";
+import * as test from "#erdstall/test";
+
+const TOKEN_SIZE = 4;
 
 describe("Deposit_Call_Wrapper", () => {
+	const rng = test.newPrng();
 	let testenv: Enviroment;
 	let bob: Wallet;
-	const amount = new Amount(100n);
+	const amount = new Amount(test.newRandomBigInt(rng, 16));
+	const tokens = test.newRandomTokens(rng, TOKEN_SIZE);
 
 	before(async () => {
 		testenv = await setupEnv();
 		bob = testenv.users[0];
+
+		const part = PerunArt__factory.connect(testenv.perunArt, bob);
+		for (const id of tokens.value) {
+			await part.mint(bob.address, id);
+		}
 	});
 
 	it("allows deposits for ETH, ERC20 and ERC721", async () => {
 		let nonce = await bob.getTransactionCount();
 
-		[
-			[makeETHDepositCalls, testenv.ethHolder, ETHZERO],
-			[makeERC20DepositCalls, testenv.erc20Holder, testenv.perun],
-		].forEach(async ([call, holder, token]) => {
-			const depCall = (call as typeof makeERC20DepositCalls)(
+		const testCases: [DepositerCallsFactory, string, string, Asset][] = [
+			[makeETHDepositCalls, testenv.ethHolder, ETHZERO, amount],
+			[makeERC20DepositCalls, testenv.erc20Holder, testenv.perun, amount],
+			[
+				makeERC721DepositCalls,
+				testenv.erc721Holder,
+				testenv.perunArt,
+				tokens,
+			],
+		];
+
+		for (const [call, holder, token, assets] of testCases) {
+			const depCall = (call as DepositerCallsFactory)(
 				bob,
-				Address.fromString(holder as string),
-				Address.fromString(token as string),
-				amount,
+				Address.fromString(holder),
+				Address.fromString(token),
+				assets,
 			);
 			for (const [_, call] of depCall) {
 				const tx = await call({ nonce: nonce++ });
@@ -45,6 +65,6 @@ describe("Deposit_Call_Wrapper", () => {
 					`${token} deposit should succeed`,
 				).to.be.equal(0x1);
 			}
-		});
+		}
 	});
 });
