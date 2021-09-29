8a67e91151609a90c281187a58b3b7da90ac84b0
Norbert Dzikowski <norbert@perun.network>
2021-09-29 14:25:08 +0200

    test: Move EnclaveMockProvider into test/mocks for consistency

--- src/test/mocks.ts
+++ src/test/mocks.ts
@@ -1,16 +1,32 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { ErdstallClient, Watcher } from "#erdstall";
-import { Mint, Trade, Transfer, Burn } from "#erdstall/api/transactions";
+import {
+	Mint,
+	Trade,
+	Transfer,
+	Burn,
+	ExitRequest,
+} from "#erdstall/api/transactions";
 import {
 	TxReceipt,
 	BalanceProof,
 	BalanceProofs,
+	ClientConfig,
+	Account as RAccount,
 } from "#erdstall/api/responses";
+import { TypedJSON } from "typedjson";
+import { Result, Call, ErdstallObject } from "#erdstall/api";
+import {
+	SubscribeTXs,
+	SubscribeBalanceProofs,
+	GetAccount,
+} from "#erdstall/api/calls";
+import { Transaction } from "#erdstall/api/transactions";
 import { Address, Account, ErdstallEvent } from "#erdstall/ledger";
 import { NFTMetadata } from "#erdstall/ledger/backend";
-import { Assets } from "#erdstall/ledger/assets";
-import { EnclaveEvent } from "#erdstall/enclave";
+import { Assets, Tokens } from "#erdstall/ledger/assets";
+import { EnclaveEvent, EnclaveProvider } from "#erdstall/enclave";
 
 export class MockWatcher implements Watcher {
 	private txReceiptHandler!: (tx: TxReceipt) => void;
@@ -99,4 +115,121 @@ export class MockClient extends MockWatcher implements ErdstallClient {
 	erdstall(): Address {
 		return this.contract;
 	}
+}
+
+export class EnclaveMockProvider implements EnclaveProvider {
+	public onopen: ((ev: Event) => any) | null;
+	public onclose: ((ev: CloseEvent) => any) | null;
+	public onerror: ((ev: Event) => any) | null;
+	public onmessage: ((ev: MessageEvent) => any) | null;
+	private config?: ClientConfig;
+
+	constructor(config?: ClientConfig) {
+		this.onopen = null;
+		this.onclose = null;
+		this.onerror = null;
+		this.onmessage = null;
+		this.config = config;
+	}
+
+	public connect() {
+		if (!this.config) return;
+
+		const msg = newErdstallMessageEvent(new Result(undefined, this.config));
+		return this.onmessage!(msg);
+	}
+
+	public send(data: string | ArrayBufferLike | Blob | ArrayBufferView) {
+		const call = TypedJSON.parse(data, Call)!;
+
+		switch (call.data.objectType()) {
+			case SubscribeTXs: {
+				const msg = newErdstallMessageEvent(new Result(call.id));
+				return this.onmessage!(msg);
+			}
+			case SubscribeBalanceProofs: {
+				const msg = newErdstallMessageEvent(new Result(call.id));
+				return this.onmessage!(msg);
+			}
+			case GetAccount: {
+				const acc = new Account(0n, new Assets(), new Assets());
+				const racc = new RAccount(acc, 0n);
+				const msg = newErdstallMessageEvent(new Result(call.id, racc));
+				return this.onmessage!(msg);
+			}
+			case Transaction: {
+				return this.respondToTX(call.id, call.data as Transaction);
+			}
+			default:
+				throw new Error("message not implemented");
+		}
+	}
+
+	private respondToTX(id: string, tx: Transaction) {
+		switch (tx.txType()) {
+			case Transfer: {
+				const txc = tx as Transfer;
+				const acc = new Account(
+					txc.nonce.valueOf(),
+					txc.values,
+					new Assets(),
+				);
+
+				const res = newTxReceiptResult(id, tx, acc);
+				const msg = newErdstallMessageEvent(res);
+				return this.onmessage!(msg);
+			}
+			case Mint: {
+				const txc = tx as Mint;
+				const assets = new Assets();
+				assets.addAsset(
+					txc.token.toString(),
+					new Tokens([txc.id.valueOf()]),
+				);
+				const acc = new Account(
+					txc.nonce.valueOf(),
+					assets,
+					new Assets(),
+				);
+
+				const res = newTxReceiptResult(id, tx, acc);
+				const msg = newErdstallMessageEvent(res);
+				return this.onmessage!(msg);
+			}
+			case ExitRequest: {
+				const res = newTxReceiptResult(id, tx);
+				const msg = newErdstallMessageEvent(res);
+				return this.onmessage!(msg);
+			}
+			default:
+				throw new Error("transaction not implemented");
+		}
+	}
+
+	public sendToClient(obj?: ErdstallObject, id?: string, error?: string) {
+		return this.onmessage!(
+			new MessageEvent("erdstall", {
+				data: TypedJSON.stringify(new Result(id, obj, error), Result),
+			}),
+		);
+	}
+
+	public close() {}
+}
+
+function newErdstallMessageEvent(res: Result): MessageEvent {
+	const data = TypedJSON.stringify(res, Result);
+	return new MessageEvent("erdstall", { data: data });
+}
+
+function newTxReceiptResult(
+	id: string,
+	tx: Transaction,
+	acc?: Account,
+): Result {
+	const _acc = acc
+		? acc
+		: new Account(tx.nonce.valueOf(), new Assets(), new Assets());
+	const txr = new TxReceipt(tx, _acc);
+	return new Result(id, txr);
 }
