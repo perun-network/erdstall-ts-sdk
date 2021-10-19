69efdb7b47cabdf91c20c29a5aa3e0264e6fc713
Steffen Rattay <steffen@perun.network>
2021-10-19 09:14:36 +0200

    test/mocks: Add optional TX delta arguments

--- src/test/mocks.ts
+++ src/test/mocks.ts
@@ -68,11 +68,14 @@ export class MockWatcher implements Watcher {
 		throw new Error("not implemented");
 	}
 
-	mint(nft: {
-		token: Address;
-		id: bigint | BigInteger;
-		owner: Address;
-	}): void {
+	mint(
+		nft: {
+			token: Address;
+			id: bigint | BigInteger;
+			owner: Address;
+		},
+		deltas?: Map<string, Account>,
+	): void {
 		const mintTx = new Mint(
 			nft.owner,
 			BigInt(0),
@@ -80,24 +83,26 @@ export class MockWatcher implements Watcher {
 			nft.id.valueOf(),
 		);
 		this.txReceiptHandler(
-			new TxReceipt(mintTx, new Map<string, Account>()),
+			new TxReceipt(mintTx, deltas ?? new Map<string, Account>()),
 		);
 	}
 
-	burn(burnTx: Burn): void {
+	burn(burnTx: Burn, deltas?: Map<string, Account>): void {
 		this.txReceiptHandler(
-			new TxReceipt(burnTx, new Map<string, Account>()),
+			new TxReceipt(burnTx, deltas ?? new Map<string, Account>()),
 		);
 	}
 
-	trade(tradeTx: Trade): void {
+	trade(tradeTx: Trade, deltas?: Map<string, Account>): void {
 		this.txReceiptHandler(
-			new TxReceipt(tradeTx, new Map<string, Account>()),
+			new TxReceipt(tradeTx, deltas ?? new Map<string, Account>()),
 		);
 	}
 
-	transfer(tx: Transfer): void {
-		this.txReceiptHandler(new TxReceipt(tx, new Map<string, Account>()));
+	transfer(tx: Transfer, deltas?: Map<string, Account>): void {
+		this.txReceiptHandler(
+			new TxReceipt(tx, deltas ?? new Map<string, Account>()),
+		);
 	}
 
 	phaseshift(bps: BalanceProofs) {
