04a29a4578967fd66b42ef1793029f8130ce5b43
Norbert Dzikowski <norbert@perun.network>
2021-10-29 17:14:07 +0200

    test: Use consistent ethers import path

--- src/test/wallet.ts
+++ src/test/wallet.ts
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import PRNG, { newRandomUint8Array } from "./random";
-import { entropyToMnemonic } from "@ethersproject/hdnode";
-import { Wallet } from "@ethersproject/wallet";
+import { utils, Wallet } from "ethers";
 
 export function newRandomWallet(rng: PRNG): Wallet {
 	const entropy = newRandomUint8Array(rng, 32);
-	const mnemonic = entropyToMnemonic(entropy);
+	const mnemonic = utils.entropyToMnemonic(entropy);
 	return Wallet.fromMnemonic(mnemonic);
 }
