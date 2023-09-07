bb8536b73d0a17b537b1bc33bb74280219f6de8b
Norbert Dzikowski <norbert@perun.network>
2023-09-07 15:01:41 +0200

    backend/ethereum: Implement EthereumSigner

--- src/ledger/backend/ethereum/signer.ts
+++ src/ledger/backend/ethereum/signer.ts
@@ -1,10 +1,26 @@
 // SPDX-License-Identifier: Apache-2.0
 "use strict";
 
-import { Signer as EthersSigner } from "ethers";
+import { Bytes, Signer as EthersSigner, utils } from "ethers";
 import { Signer } from "#erdstall/ledger/backend";
+import { Signature } from "#erdstall/ledger/signature";
+import { Address } from "#erdstall/ledger";
+import { EthereumSignature } from "./signature";
 
 // Compile-time check that the EthersSigner implements the Signer interface.
-export abstract class EthereumSigner
-	extends EthersSigner
-	implements Signer<"ethereum"> {}
+export class EthereumSigner implements Signer<"ethereum"> {
+	readonly address: Address<"ethereum">;
+	readonly ethersSigner: EthersSigner;
+
+	constructor(address: Address<"ethereum">, ethersSigner: EthersSigner) {
+		this.address = address;
+		this.ethersSigner = ethersSigner;
+	}
+
+	async signMessage(message: string | Bytes): Promise<Signature<"ethereum">> {
+		const sig = await this.ethersSigner.signMessage(
+			utils.keccak256(message),
+		);
+		return new EthereumSignature(utils.arrayify(sig), this.address);
+	}
+}
