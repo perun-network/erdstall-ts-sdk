f9904756f6cd2ca6eba924695e206f9292444d93
Norbert Dzikowski <norbert@perun.network>
2023-10-06 02:32:00 +0200

    ledger/backend: Add Encoder abstraction for sig-verification

--- src/ledger/backend/encoder.ts
+++ src/ledger/backend/encoder.ts
@@ -2,11 +2,22 @@
 "use strict";
 
 import { Backend } from "#erdstall/ledger/backend/backends";
-import { BalanceProof } from "#erdstall/api/responses";
+import { Address } from "#erdstall/ledger/address";
+import { Chain } from "../chain";
+import { ChainProof } from "#erdstall/api/responses";
 
-export interface Encoder<_B extends Backend> {
-	// Encodes the given BalanceProof into a byte array. The resulting
-	// encoding is the backend specific representation of proofs used for on-chain
-	// verification & validation.
-	encode(proof: BalanceProof): Uint8Array;
+export interface Encoder<B extends Backend> {
+	// Encodes the given ChainProofDesc into a signable representation for the
+	// instantiating backend. The resulting Uint8Array can be used to verify
+	// that the proofs were indeed issued by enclave running Erdstall.
+	encode(desc: ChainProofDesc<B>): Uint8Array;
 }
+
+// Descriptor for ChainProofs tying together the address and chain for which
+// the given ChainProof was signed.
+export type ChainProofDesc<B extends Backend> = {
+	address: Address<B>;
+	epoch: bigint;
+	chain: Chain;
+	proofs: ChainProof;
+};
