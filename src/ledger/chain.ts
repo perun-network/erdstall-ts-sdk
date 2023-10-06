5169c7e69039db721b560462e4a1a8736c7ed0ff
Norbert Dzikowski <norbert@perun.network>
2023-10-06 02:29:18 +0200

    ledger: Update Chain enum to include all Erdstall supported chains

--- src/ledger/chain.ts
+++ src/ledger/chain.ts
@@ -3,6 +3,26 @@
 
 export enum Chain {
 	Erdstall = 0,
-	Ethereum = 1,
-	Polkadot = 2,
+
+	// Production chains.
+	EthereumMainnet = 1,
+	Ajuna = 1328,
+	Bajun = 1337,
+
+	// Generic testnet chains start at half of the uint16 range.
+	TestChain0 = 0x8000,
+	TestChain1,
+	TestChain2,
+	TestChain3,
+	TestChain4,
+	TestChain5,
+	TestChain6,
+	TestChain7,
+	TestChain8,
+	TestChain9,
+
+	// Special testnet chains.
+	Goerli = 0xffff - 5,
+	Ropsten = 0xffff - 1,
+	Rinkeby = 0xffff - 0,
 }
